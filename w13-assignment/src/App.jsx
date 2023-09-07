a0f38c021f30c5215524bb67f776d0e68e6274d2
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-07 14:24:07 -0600

    Photo and Welcome statement added.

--- w13-assignment/src/App.jsx
+++ w13-assignment/src/App.jsx
@@ -1,9 +1,14 @@
+// imports components from other files in the src directory.
 import Navigation from './Nav.jsx'
+import Login from './login.jsx'
 
+
+// Export default allows the app component to be called in the main.jsx file.
 export default function App() {
   return (
-    <div>
-    <Navigation></Navigation>
+    <div className='w-screen h-screen'>
+      <Navigation></Navigation>
+      <Login></Login>
     </div>
   )
 }
--- w13-assignment/src/login.jsx
+++ w13-assignment/src/login.jsx
@@ -1,9 +1,17 @@
 import React, {Component} from 'react'
+import peruImage from './assets/peru-react.jpg';
 
 export default class Login extends Component {
     render() {
         return (
-            div.
+            <body className=''>
+                <main className="">
+                    <div className="flex w-1/2 h-screen" id="">
+                        <h1 className="">Welcome to Travel Bound</h1>
+                        <img src={peruImage} className="rounded-2xl" alt="Photo of a mother and her daughter waving somewhere in Peru." />
+                    </div>
+                </main>    
+            </body>
         )
     }
 }
