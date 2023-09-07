a75627eb4fdd830597522ff846adca604ecc5185
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-07 17:20:17 -0600

    Update before closing program.

--- w13-assignment/src/Nav.jsx
+++ w13-assignment/src/Nav.jsx
@@ -4,10 +4,10 @@ export default class Nav extends Component {
     render() {
         return (
             <nav className="text-3xl w-screen h-32 bg-cyan-600 flex p-4" id="">
-                <div className="p-8 items-center justify-start">
+                <div className="p-8 ml-20 items-center justify-start">
                     <h1>Travel Bound Co</h1>
                 </div>
-                <div className="p-12 flex items-center justify-end">
+                <div className="p-12 ml-auto mr-20 flex items-center justify-end">
                     <ul className="flex flex-row space-x-6">
                         <li className="">Home</li>
                         <li className="">Destinations</li>
