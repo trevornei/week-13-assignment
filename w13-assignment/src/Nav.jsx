1274a4ad7d28243735a0036b338bc81862fa5797
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-07 11:22:53 -0600

    Vertically and horizontally centered nav logo and ul.

--- w13-assignment/src/Nav.jsx
+++ w13-assignment/src/Nav.jsx
@@ -3,9 +3,16 @@ import React, {Component} from 'react'
 export default class Nav extends Component {
     render() {
         return (
-            <nav className="text-3xl w-screen h-12 bg-cyan-600" id="">
-                <div className="">
-                    
+            <nav className="text-3xl w-screen h-32 bg-cyan-600 flex p-4" id="">
+                <div className="p-8 items-center justify-start">
+                    <h1>Travel Bound Co</h1>
+                </div>
+                <div className="p-12 flex items-center justify-end">
+                    <ul className="flex flex-row space-x-6">
+                        <li className="">Home</li>
+                        <li className="">Destinations</li>
+                        <li className="">Login</li>
+                    </ul>
                 </div>
             </nav>
         )
