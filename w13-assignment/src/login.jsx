02e99a6392a8a0c78387e2c9548875340047bb9c
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-07 15:08:58 -0600

    Added font colors to label and input.

--- w13-assignment/src/login.jsx
+++ w13-assignment/src/login.jsx
@@ -4,22 +4,18 @@ import peruImage from './assets/peru-react.jpg';
 export default class Login extends Component {
     render() {
         return (
-            <body className=''>
+            <body className='bg-cyan-100'>
                 <main className="">
-                    {
-                        // Div that contains the welcome photo and login.
-                    }
-                    <div className="w-screen h-screen">
-                        {/* <div className="flex w-1/2 h-screen" id="">
-                            <h1 className="">Welcome to Travel Bound</h1>
-                            <img src={peruImage} className="rounded-2xl" alt="Photo of a mother and her daughter waving somewhere in Peru." />
-                        </div> */}
                         <div className="z-100 w-screen h-screen flex items-center justify-center" id="login-cont">
-                            <div className="bg-cyan-700 w-screen h-96 flex flex-col items-center justify-center m-64">
+                            <div className="bg-cyan-700 w-96 h-96 flex flex-col items-center justify-center rounded-3xl m-64">
+                                <div className="w-1/4 h-12 flex items-center justify-center border-solid border-2 border-cyan-300 shadow-md shadow-cyan-100">
+                                    <h3 className='text-3xl text-cyan-200'>Login</h3>
+                                </div>
+                                
                                 {
                                     //Username
                                 }
-                                    <div className="m-4">
+                                    <div className="m-4 text-cyan-300">
                                         <label htmlFor="" className="" id="">
                                             <h6>Username</h6>
                                         </label>
@@ -28,31 +24,21 @@ export default class Login extends Component {
                                 {
                                     // Password
                                 }
-                                    <div className="m-4">
+                                    <div className="m-4 text-cyan-300">
                                         <label htmlFor="" className="" id="">
                                             <h6>Password</h6>
                                         </label>
                                         <input type="text" className="" id=""/>
                                     </div>
                                 {
-                                    // Am I a robot??
-                                }
-                                <div className="m-4">
-                                    <label htmlFor="" className="" id="">
-                                        <h6>Are you human?</h6>
-                                    </label>
-                                    <input type="chekcbox" className="" id=""/>
-                                </div>
-                                {
                                     // Button
                                 }
-                                <button className="w-32 h-10 bg-cyan-100 rounded-3xl my-4" id="">
+                                <button className="w-32 h-10 bg-cyan-100 rounded-3xl my-4" id="" type='submit'>
                                     <h3>Submit</h3>
                                 </button>
 
                             </div>
                         </div>
-                    </div>
                 </main>    
             </body>
         )
